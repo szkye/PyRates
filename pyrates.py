29ebc7ba9e213acb927890ee2f1fd37e7288e891
szkye <artyomszkye@proton.me>
2023-11-19 23:45:24 +0100

    fixes

--- pyrates.py
+++ pyrates.py
@@ -1,16 +1,7 @@
 import requests
 import click
 
-def list_rates(ctx, param, value):
-    if not value or ctx.resilient_parsing:
-        return
-    key = "https://open.er-api.com/v6/latest/"
-    first = 'EUR'
-    data = requests.get(key+first)
-    data = data.json()
-
 @click.command()
-@click.option('-l', is_flag=True, callback=list_rates, expose_value=False, is_eager=True)
 @click.argument('amount', nargs=1, type=click.FLOAT)
 @click.argument('first', nargs=1, type=click.STRING)
 @click.argument('second', nargs=1, type=click.STRING)
